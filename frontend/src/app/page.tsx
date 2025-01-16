ea41912063e0cde78cc5ca9736f84cb9dc2e8aad
Ivan Ruiz <maoruiz24@hotmail.com>
2025-01-16 16:19:42 -0500

    change on deploy actions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-4xl font-bold text-center">Hello, TypeScript Next.js!</h1>
       <p className="text-lg text-center">
-        This is a simple Next.js app with TypeScript, Tailwind CSS, and ESL
+        This is a simple Next.js app with TypeScript, Tailwind CSS, and ESL.
         integration.
         </p>
     </div>
